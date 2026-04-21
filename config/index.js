f37af03e681f8c3d7c0d1129072de925b656bda1
Rahul Rudra <msrrahul77@gmail.com>
2026-04-21 11:29:19 +0600

    code changes made

---
+++ config/index.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+export default {
+    PORT:process.env.PORT,
+    DB_USER:process.env.DB_USER,
+    DB_PASSWORD:process.env.DB_PASSWORD,
+
+}
---
+++ index.js
@@ -0,0 +1,213 @@
+import bodyParser from "body-parser";
+import cors from "cors"
+import express from "express"
+
+import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
+import config from './config/index.js';
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json());
+const port=config.PORT||8000
+
+app.use(bodyParser.urlencoded({ extended: true }));
+const uri = `mongodb+srv://${config.DB_USER}:${config.DB_PASSWORD}@cluster0.rprg1ag.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+
+      const serviceCollection = await client.db("dillBaz").collection('services')
+
+
+
+
+    const priceCollection = await client.db("dillBaz").collection('prices')
+
+      const teamCollection = await client.db("dillBaz").collection('teams')
+    app.get('/services', async (req, res) =>
+    {
+        const query = {}
+        const cursour = serviceCollection.find(query)
+        const skillData = await cursour.toArray();
+        res.send(skillData)
+        })
+
+    app.get('/services/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to fetch service' });
+    }
+});
+      app.post("/services", async (req, res) => {
+        try {
+            const data = req.body
+
+          const result = await serviceCollection.insertOne(data)
+          console.log(result)
+          res.status(200).json({
+              data:result
+          })
+
+        } catch (error) {
+          console.log(error)
+        }
+      })
+app.patch('/services/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        if (!ObjectId.isValid(id)) {
+            return res.status(400).send({ error: 'Invalid ID format' });
+        }
+
+        const filter = { _id: new ObjectId(id) };
+        const updateData = req.body;
+
+        const updateDoc = {
+            $set: updateData
+        };
+
+        const result = await serviceCollection.updateOne(filter, updateDoc);
+
+        if (result.matchedCount === 0) {
+            return res.status(404).send({ error: 'Service not found' });
+        }
+
+        res.send({
+            message: 'Service updated successfully',
+            result
+        });
+
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to update service' });
+    }
+});
+app.delete('/services/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+
+        const result = await serviceCollection.deleteOne(query);
+
+        if (result.deletedCount === 0) {
+            return res.status(404).send({ error: 'Service not found' });
+        }
+
+        res.send({ message: 'Service deleted successfully', result });
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to delete service' });
+    }
+});
+
+
+
+
+
+ app.get('/teams', async (req, res) =>
+    {
+        const query = {}
+        const cursour = teamCollection.find(query)
+        const teamData = await cursour.toArray();
+        res.send(teamData)
+        })
+
+    app.get('/teams/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const teams = await teamCollection.findOne(query);
+        res.send(teams);
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to fetch Teams' });
+    }
+});
+      app.post("/teams", async (req, res) => {
+        try {
+            const data = req.body
+
+          const result = await teamCollection.insertOne(data)
+          console.log(result)
+          res.status(200).json({
+              data:result
+          })
+
+        } catch (error) {
+          console.log(error)
+        }
+      })
+app.patch('/teams/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        if (!ObjectId.isValid(id)) {
+            return res.status(400).send({ error: 'Invalid ID format' });
+        }
+
+        const filter = { _id: new ObjectId(id) };
+        const updateData = req.body;
+
+        const updateDoc = {
+            $set: updateData
+        };
+
+        const result = await teamCollection.updateOne(filter, updateDoc);
+
+        if (result.matchedCount === 0) {
+            return res.status(404).send({ error: 'Team Data not found' });
+        }
+
+        res.send({
+            message: 'Team updated successfully',
+            result
+        });
+
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to update service' });
+    }
+});
+app.delete('/teams/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+
+        const result = await teamCollection.deleteOne(query);
+
+        if (result.deletedCount === 0) {
+            return res.status(404).send({ error: 'Service not found' });
+        }
+
+        res.send({ message: 'Team Data deleted successfully', result });
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to delete service' });
+    }
+});
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log("Dill Bazz server is runnig in port : ", port);
+})
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
